5d11605357f38bd946a670f322d5df10bdf8c56e
Augasty <sayaksengupta140@gmail.com>
2024-03-09 16:07:56 +0530

    header done

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,34 @@
+<script>
+	import "../app.pcss";
+    import Header from './Header.svelte';
+    import './styles.css';
+</script>
+
+<div class="app">
+	<Header></Header>
+
+	<main>
+		<slot></slot>
+	</main>
+</div>
+
+<style>
+	.app {
+		display: flex;
+		flex-direction: column;
+		min-height: 100vh;
+	}
+
+	main {
+		flex: 1;
+		display: flex;
+		flex-direction: column;
+		padding: 1rem;
+		width: 100%;
+		max-width: 64rem;
+		margin: 0 auto;
+		box-sizing: border-box;
+	}
+
+
+</style>
---
+++ src/routes/Header.svelte
@@ -0,0 +1,32 @@
+<script>
+	import { Button } from '$lib/components/ui/button';
+	import * as Avatar from '$lib/components/ui/avatar';
+	import { Home, AlignStartVertical, BookOpen, SquarePlus, ChevronDown } from 'lucide-svelte';
+	import profilepic from '../lib/images/profilepic.jpeg';
+</script>
+
+<header class="font-inter">
+	<div class="mt-2 flex">
+		<Button variant="ghost" class="mr-4 text-xs"
+			><Home size="12" strokeWidth="1" class="mr-2" /> Home</Button
+		>
+
+		<Button variant="ghost" class="mr-4 text-xs"
+			><AlignStartVertical size="12" strokeWidth="1" class="mr-2" /> Explore</Button
+		>
+
+		<Button variant="ghost" class="mr-4 text-xs"
+			><BookOpen size="12" strokeWidth="1" class="mr-2" /> Learn</Button
+		>
+
+		<Button variant="outline" class="mr-12 border border-b-4 border-black text-xs"
+			><SquarePlus size="12" strokeWidth="1" class="mr-2" /> Post</Button
+		>
+
+		<Avatar.Root  class="mr-4">
+			<Avatar.Image src={profilepic} alt="@shadcn" class="rounded-full border border-black" />
+			<Avatar.Fallback class="rounded-full border border-black">KK</Avatar.Fallback>
+		</Avatar.Root>
+		<div  class="flex items-center">Krishna Kiran <ChevronDown size="16" strokeWidth="1" class="ml-2"/></div>
+	</div>
+</header>
